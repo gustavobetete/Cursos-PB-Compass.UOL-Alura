fca61caff835a9b3baaf809bef53122f1c3ca077
Gustavo Betete <gustavobetete@hotmail.com>
2022-03-16 12:03:27 -0300

    Upload Sprints

---
+++ Sprint 03/5 - Spring Boot API REST Segurança da API/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,20 @@
+package br.com.alura.forum.repository;
+
+import br.com.alura.forum.modelo.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.yaml.snakeyaml.events.Event;
+
+import java.util.List;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+
+    Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
+
+//    @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+//    List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+}
